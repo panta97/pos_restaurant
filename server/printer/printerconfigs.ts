006f2434a842e31be3ec7b74802e7e7f73f75c9d
Roosevelt <panta_85.x@hotmail.com>
2021-04-24 19:25:51 -0500

    added default chartset

--- server/printer/printerconfigs.ts
+++ server/printer/printerconfigs.ts
@@ -1,10 +1,12 @@
 import NTP from "node-thermal-printer";
 
 const timeout = Number(process.env.PRINTER_TIMEOUT!);
+const characterSet = "WPC1252";
 
 export const TEST_PRINTER = {
   type: NTP.types.EPSON,
   interface: "tcp://192.168.0.51",
+  characterSet,
   options: {
     timeout,
   },
@@ -13,6 +15,7 @@ export const TEST_PRINTER = {
 export const BAR_PRINTER = {
   type: NTP.types.EPSON,
   interface: `tcp://${process.env.PRINTER_IP_BAR!}`,
+  characterSet,
   options: {
     timeout,
   },
@@ -21,6 +24,7 @@ export const BAR_PRINTER = {
 export const RESTAURANT_PRINTER = {
   type: NTP.types.EPSON,
   interface: `tcp://${process.env.PRINTER_IP_REST!}`,
+  characterSet,
   options: {
     timeout,
   },
